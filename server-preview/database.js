215ce70ddadc72f9f4a0033d2703945d9f0077a9
ahmadmaaz <66618062+ahmadmaaz@users.noreply.github.com>
2022-12-06 19:18:25 +0200

    Update database.js

--- server-preview/database.js
+++ server-preview/database.js
@@ -1,17 +1,10 @@
 const mysql= require("mysql2")
-module.exports= mysql.createConnection({
-    host:"eu-cdbr-west-02.cleardb.net",
-    user:"b6f9d8b7587991",
-    password:"293481c1",
-    database:'heroku_5191ad6e3721447',
-    port:'3306'
-})
-/*
+
+
 module.exports= mysql.createConnection({
     host:"127.0.0.1",
     port:"3300",
     user:'root',
-    password:"DC8E6A353Mmaaz",
+    password:"",
     database:"questions"
 })
-**/
